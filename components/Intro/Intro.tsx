a040dc74848656fa349f32f884b56fb4130d57f3
PaulSamPS <psamoylenko2@gmail.com>
2022-05-31 04:42:51 +0500

    удалил кавычке в интро

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -15,7 +15,7 @@ export const Intro = forwardRef(({ className, ...props }: IntroType, ref: Forwar
         <P>
           Artificial intelligence (AI), sometimes called machine intelligence, is intelligence demonstrated by machines,
           unlike the natural intelligence displayed by humans and animals. Leading AI textbooks define the field as the
-          study of "intelligent agents": any device that perceives its environment and takes actions that maximize its
+          study of intelligent agents: any device that perceives its environment and takes actions that maximize its
           chance of successfully achieving its goals.
         </P>
       </div>
