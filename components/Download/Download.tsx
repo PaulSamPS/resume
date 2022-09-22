e73c4404a7c19d1ae042adc6c8c4c8ed96731b2e
PaulSamPS <19071990Pqw>
2022-09-23 01:07:56 +0500

    переместил резюме в папку public

--- components/Download/Download.tsx
+++ components/Download/Download.tsx
@@ -5,14 +5,12 @@ import { saveAs } from 'file-saver';
 
 export const Download = forwardRef((props, ref: ForwardedRef<HTMLDivElement>) => {
   const saveFile = () => {
-    saveAs('http://176.113.83.209:5000/resume.txt', 'resume.txt');
+    saveAs('/resume.txt', 'resume.txt');
   };
   return (
     <div className={styles.wrapper} ref={ref}>
       <div className={styles.download}>
-        <H appearance='uppercase' onClick={saveFile}>
-          Скачать резюме
-        </H>
+        <a onClick={saveFile}>Скачать резюме</a>
       </div>
     </div>
   );
